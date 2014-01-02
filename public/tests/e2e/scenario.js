8be90b648f2dbbbc7197041a9273a143434c484a
natehershey <natehershey@gmail.com>
2014-01-01 23:37:11 -0700

    Tests

---
+++ public/tests/e2e/scenario.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+
+describe('Shopping lists', function() {
+  beforeEach(function() {
+    browser().navigateTo('/');
+    sleep(1);
+  });
+
+  it("should list the available shopping lists and their categories", function() {
+    expect(repeater('.shopping-list').count()).toBe(2);
+    // expect(repeater('.done').count()).toBe(1);
+
+    // element('.not-done:nth-child(1) input').click();
+    // sleep(0.1);
+    // expect(repeater('.not-done').count()).toBe(1);
+    // expect(repeater('.done').count()).toBe(2);
+
+    // element('.done:nth-child(1) input').click();
+    // sleep(0.1);
+    // expect(repeater('.not-done').count()).toBe(2);
+    // expect(repeater('.done').count()).toBe(1);
+  });
+});
